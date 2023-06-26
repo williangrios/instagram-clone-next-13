bb49ca62ec921daea05c834b93d7215239c702d9
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-06-26 07:57:26 -0300

    fixing bugs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,11 +17,10 @@ interface CustomUser extends DefaultSession {
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.SECRET,
   pages: {
     signIn: "/signin",
   },
